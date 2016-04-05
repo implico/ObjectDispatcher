51d8b39be5afeaa10df704f91d3b6939489ba501
Implico <bartosz.sak@implico.pl>
2016-04-05 04:50:55 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # objectDispatcher
-Simple object module dispatcher
+Simple object module dispatcher. Performs a recursive loop over the object properties.
+
+## Example
+
+"test: yes" will appear, the other not.
+
+```js
+var OD = new ObjectDispatcher('app');
+OD.module('index', {
+
+	//no _init function for top-level: checked if module with id = index exists (by default: #module-[moduleId], in this case #module-index)
+
+	test: function() {
+		console.log('test: yes');
+	},
+
+	//starting with _ - no automatic dispatch
+	_testNo: function() {
+		console.log('test: no');
+	},
+
+	//submodule
+	testIndex: {
+		//manual check - if returns truthy value, becomes dispatched
+		_init: function() {
+			return OD.isModule('index');	//checks if an element that matches #module-index selector exists
+		},
+		testSub41: function() {
+			console.log('testsubIndex: yes');
+		}
+	},
+
+	//submodule
+	testSub: {
+		//no _init for lower level: always dispatched
+
+		variable: 1,
+
+		testSub1: function() {
+			console.log('testSub1: yes');
+		},
+		testSub2: function() {
+			console.log('testSub2: yes');
+		},
+		testSub3: {
+			_init: function() {
+				this._testSub32();
+			},
+			testSub31: function() {
+				console.log('testsub31: no');
+			},
+			_testSub32: function() {
+				console.log('_testSub32: yes');
+			}
+		},
+		testSub4: {
+			_init: function() {
+				return OD.isModule(['not-exists', 'not-exists-2']);
+			},
+			testSub41: function() {
+				console.log('testsub41: no');
+			}
+		}
+	},
+
+	//submodule as a closure with "private" variables
+	testMod: (function() {
+		var variable = 1;
+
+		return {
+			testMod1: function() {
+				console.log('testmod1: yes', variable++);
+			},
+			_testMod2: function() {
+				console.log('testmod2: no');
+			},
+			testMod3: function() {
+				console.log('testmod3: yes', variable);
+			}
+		}
+	})()
+});
+
+//run dispatcher
+OD.dispatch();
+```
+
+Assuming HTML:
+
+```html
+...
+<div id="module-index">
+	...
+</div>
+...
+```
+
+## Options
+
+Pass as a second argument to the constructor.
+
+```js
+//shows default options
+var options = {
+	//module check options
+	isModule: {
+		//pass a function to replace standard behavior
+		body: false,
+		//prepended to tested selector
+		prepend: '#module-',
+		//appended to tested selector
+		append: ''
+	}
+}
+```
+
+See more in the source comments.
---
+++ dist/objectDispatcher.js
@@ -0,0 +1,166 @@
+/**
+	Object dispatcher
+
+	https://github.com/implico/objectDispatcher
+
+	@author bartosz.sak@gmail.com
+	@license MIT
+	
+*/
+(function(window, document) {
+
+	function ObjectDispatcher(id, options) {
+		var _this = this;
+		var defaults = {
+			//module check options
+			isModule: {
+				//pass a function to replace standard behavior
+				body: false,
+				//prepended to tested selector
+				prepend: '#module-',
+				//appended to tested selector
+				append: ''
+			}
+		}
+		this.settings = deepExtend({}, defaults, options);
+
+		if ((typeof id != 'string') || (id.length < 1)) {
+			throw new OdException('module id must be a non-empty string');
+		}
+		this.id = id;
+		this.modules = {};
+		ObjectDispatcher.apps[id] = this;
+	}
+
+	ObjectDispatcher.apps = {};
+
+	//static function, returns app by id
+	ObjectDispatcher.app = function(appId) {
+		if (ObjectDispatcher.apps[appId] instanceof ObjectDispatcher) {
+			return ObjectDispatcher.apps[appId];
+		}
+		else {
+			throw new OdException('application ' + appId + 'not found');
+		}
+	}
+
+	//sets or gets a module
+	ObjectDispatcher.prototype.module = function(moduleId, moduleBody) {
+		if (typeof moduleId === 'undefined') {
+			throw new OdException('undefined module id');
+		}
+		else if (typeof moduleBody === 'undefined') {
+			if (this.modules[moduleId]) {
+				return this.modules[moduleId];
+			}
+			else {
+				throw new OdException('module ' + moduleId + ' not found');
+			}
+		}
+		else if (moduleBody && (typeof moduleBody === 'object')) {
+			this.modules[moduleId] = moduleBody;
+			return moduleBody;
+		}
+		else {
+			throw new OdException('wrong module body type, expected object, ' + typeof moduleBody + ' given');
+		}
+	}
+
+	//returns true if an element with id (string) or one of ids (Array) is found; pass force not to prepend and append anything to the selector
+	ObjectDispatcher.prototype.isModule = function(id, force) {
+    var replaceFn = this.settings.isModule.body;
+    if (replaceFn) {
+    	if (typeof replaceFn !== 'function')
+    		throw new OdException('expected function, ' + typeof replaceFn + ' given');
+    	return replaceFn.call(this, id);
+    }
+    if (!force) {
+    	id = this.settings.isModule.prepend + id + this.settings.isModule.append;
+    }
+
+    var isCurrent = false;
+    var ids = id instanceof Array ? id : [id];
+    for (var i = 0; i < ids.length; i++) {
+      if (document.querySelectorAll(ids[i]).length) {
+        isCurrent = true;
+        id = ids[i];
+        break;
+      }
+    }
+    return isCurrent ? id : false;
+  }
+
+  //performs dispatching
+  //for top-level modules, if _init function is not set, an ObjectDispatcher.isModule(moduleId) is assumed
+  //if _init function is set, must return truthy value to start function walk
+  //if _init function isnot set or returns a truthy value, the function walk starts
+  //functions with keys starting with _ are skipped in function walk
+	ObjectDispatcher.prototype.dispatch = function(obj, objKey, depth, moduleId) {
+		if (typeof obj === 'undefined') {
+			this.dispatch(this.modules, null, 0);
+		}
+		else if (!obj || (typeof obj !== 'object')) {
+			throw new OdException('expected object, ' + typeof obj + ' given (moduleId: ' + moduleId + ', key: ' + objKey + ')');
+		}
+		else if 	((depth == 0)
+						|| ((depth == 1) && (typeof obj['_init'] === 'undefined') && this.isModule(objKey))
+						|| ((depth > 1) && (typeof obj['_init'] === 'undefined'))
+						|| ((typeof obj['_init'] === 'function') && obj['_init']())
+						|| ((typeof obj['_init'] !== 'function') && obj['_init'])) {
+			for (var key in obj) {
+				if (!obj.hasOwnProperty(key) || (key == '_init'))
+					continue;
+				if (obj[key] && (typeof obj[key] === 'object')) {
+					this.dispatch(obj[key], key, depth + 1, depth == 0 ? key : moduleId);
+				}
+				else if (typeof obj[key] === 'function') {
+					if ((typeof key !== 'string') || (key.charAt(0) != '_'))
+						obj[key]();
+				}
+			}
+		}
+	}
+
+
+	/*
+	 *	Utilities
+	 */
+
+	//Exception object
+	function OdException(message) {
+		this.message = 'ObjectDispatcher: ' + message;
+	}
+
+	OdException.prototype = Object.create(Error.prototype);;
+	OdException.prototype.constructor = OdException;
+
+
+	//$.extend in Vanilla JS
+	//source: http://youmightnotneedjquery.com/
+	var deepExtend = function(out) {
+	  out = out || {};
+
+	  for (var i = 1; i < arguments.length; i++) {
+	    var obj = arguments[i];
+
+	    if (!obj)
+	      continue;
+
+	    for (var key in obj) {
+	      if (obj.hasOwnProperty(key)) {
+	        if (typeof obj[key] === 'object')
+	          out[key] = deepExtend(out[key], obj[key]);
+	        else
+	          out[key] = obj[key];
+	      }
+	    }
+	  }
+
+	  return out;
+	}
+
+
+	//make the "class" global
+	window.ObjectDispatcher = ObjectDispatcher;
+
+})(window, document);
