28cce1e154ab0f74b0b9e6702e3b47056bb1516c
Implico <bartosz.sak@implico.pl>
2016-05-18 00:31:52 +0200

    init on dom ready, set module id in dispatch method, module paths, system properties: __module, __modules, __parent, __od; passing module object to dispatched functions, __init can be a string

--- dist/objectDispatcher.js
+++ dist/objectDispatcher.js
@@ -7,11 +7,20 @@
   @license MIT
 
 */
+
+'use strict';
+
 (function(window, document) {
 
   function ObjectDispatcher(id, options) {
     var _this = this;
     var defaults = {
+      dispatch: {
+        //the dispatch is run on DOMContentLoaded - this will additionally cause to wait until all modules are defined
+        domReady: true,
+        //the initial dispatch run is wrapped in setTimeout(fn, queue) to force run at the end of the queue
+        queue: false
+      },
       //module check options
       isModule: {
         //pass a function to replace standard behavior
@@ -46,21 +55,55 @@
     }
   }
 
+  //returns module reference by slash-delimited path (and creates if not exists)
+  ObjectDispatcher.prototype.getModuleRefByPath = function(path, create) {
+    if (typeof path !== 'string') {
+      throw new OdException('wrong module name/path, expected string, ' + typeof path + ' given');
+    }
+    var pathArr = path.split('/'),
+        ref = this.modules,
+        curKey;
+
+    for (var i = 0; i < pathArr.length; i++) {
+      curKey = pathArr[i];
+
+      if (create && (i == (pathArr.length - 1))) {
+        ref = { object: ref, key: curKey };
+      }
+      else if (typeof ref[curKey] === 'undefined') {
+        if (create) {
+          ref[curKey] = {};
+          ref = ref[curKey];
+        }
+        else {
+          return undefined;
+        }
+      }
+      else {
+        ref = ref[curKey];
+      }
+    }
+
+    return ref;
+  }
+
   //sets or gets a module
   ObjectDispatcher.prototype.module = function(moduleId, moduleBody) {
     if (typeof moduleId === 'undefined') {
       throw new OdException('undefined module id');
     }
     else if (typeof moduleBody === 'undefined') {
-      if (this.modules[moduleId]) {
-        return this.modules[moduleId];
+      var module = this.getModuleRefByPath(moduleId);
+      if (typeof module !== 'undefined') {
+        return module;
       }
       else {
         throw new OdException('module ' + moduleId + ' not found');
       }
     }
     else if (moduleBody && (typeof moduleBody === 'object')) {
-      this.modules[moduleId] = moduleBody;
+      var module = this.getModuleRefByPath(moduleId, true);
+      module.object[module.key] = moduleBody;
       return moduleBody;
     }
     else {
@@ -69,9 +112,9 @@
   }
 
   //returns true if an element with id (string) or one of ids (Array) is found; pass force not to prepend and append anything to the selector
-  ObjectDispatcher.prototype.isModule = function(id, force) {
+  ObjectDispatcher.prototype.isModule = function(id, force, useOriginal) {
     var replaceFn = this.settings.isModule.fn;
-    if (replaceFn) {
+    if (!useOriginal && replaceFn) {
       if (typeof replaceFn !== 'function')
         throw new OdException('expected function, ' + typeof replaceFn + ' given');
       return replaceFn.call(this, id, force);
@@ -93,57 +136,116 @@
   }
 
 
-  //performs dispatching
-  //by default, the initial run is wrapped in setTimeout(0) to force run at the end of the queue; pass false to start dispatching instantly
+  //starts dispatching
 
-  //if __init function is set, must return truthy value to start dispatching
-  //for top-level modules: if __init function is not set, return value of this.isModule(moduleId) is assumed
-  //for submodules (lower-level): if __init function is not set, true is assumed
+  //if __init property is set, to start dispatching:
+  // - function: must return truthy value
+  // - boolean value: must be true
+  // - string: this.isModule(__init) is checked
+  //for top-level modules: if __init property is not set, return value of this.isModule(moduleId) is assumed
+  //for submodules (lower-level): if __init property is not set, true is assumed
   //functions and objects with keys starting with "_" are skipped in dispatching
   //if you provide a __once property with true value, the module will be initialized only once (e.g. for SPA layout code)
-  ObjectDispatcher.prototype.dispatch = function(useTimeout, data, obj, objKey, depth, moduleId) {
+  ObjectDispatcher.prototype.dispatch = function(options) {
     var _this = this;
-    if (typeof obj === 'undefined') {
-      if (useTimeout) {
-        setTimeout(function() {
-          _this.dispatch(false, data, _this.modules, null, 0);
-        }, 0);
-      }
-      else this.dispatch(false, data, _this.modules, null, 0);
 
-      return this;
+    var defaults = {
+      data: undefined,
+      moduleId: null,
+      force: false,
+      options: _this.settings
     }
-    else if (!obj || (typeof obj !== 'object')) {
+
+    var options = options || {};
+
+    var settings = deepExtend({}, defaults, options);
+
+    //todo: force
+    var dispatchFn = function() {
+      if (settings.moduleId !== null) {
+        //module id set
+        _this.dispatchRun(_this.module(settings.moduleId), settings.data, settings.force, 1, settings.moduleId, settings.moduleId);
+      }
+      else {
+        //all modules
+        _this.dispatchRun(_this.modules, settings.data, settings.force, 0);
+      }
+    }
+
+    var queue = settings.options.dispatch.queue,
+        dispatchFnQueued = dispatchFn;
+    //apply timeout
+    if (queue !== false) {
+      dispatchFnQueued = function() {
+        setTimeout(dispatchFn, queue === true ? 0 : queue);
+      }
+    }
+
+    if (settings.options.dispatch.domReady) {
+      onDomReady(dispatchFnQueued);
+    }
+    else {
+      dispatchFnQueued();
+    }
+  }
+
+  //performs dispatching
+  //ObjectDispatcher.prototype.dispatchRun = function(useTimeout, data, obj, objKey, depth, moduleId) {
+  ObjectDispatcher.prototype.dispatchRun = function(obj, data, force, depth, objKey, moduleId, module, parentModule) {
+    var _this = this;
+
+    if (!obj || (typeof obj !== 'object')) {
       throw new OdException('expected object, ' + typeof obj + ' given (moduleId: ' + moduleId + ', key: ' + objKey + ')');
     }
     else if   ((depth == 0)
+            || (force)
             || ((depth == 1) && (typeof obj['__init'] === 'undefined') && this.isModule(objKey))
+            || ((depth == 1) && ((typeof obj['__init'] === 'string') || (obj['__init'] instanceof Array)) && this.isModule(obj['__init']))
             || ((depth > 1) && (typeof obj['__init'] === 'undefined'))
             || ((typeof obj['__init'] === 'function') && obj['__init'](data))
-            || ((typeof obj['__init'] !== 'function') && obj['__init'])) {
+            || ((typeof obj['__init'] !== 'function') && (obj['__init'] === true))) {
+
+      if (depth == 1) {
+        module = obj;
+      }
 
       if ((typeof obj.__once !== 'undefined') && (typeof obj.__once !== 'number'))
         obj.__once = obj.__once ? 1 : 0;
 
-      if ((depth == 0) || (typeof obj.__once === 'undefined') || (--obj.__once >= 0)) {
+      if ((depth == 0) || (force) || (typeof obj.__once === 'undefined') || (--obj.__once >= 0)) {
         if (obj.__once && (obj.__once < 0))
           obj.__once = 0;
+
+        //set system refs
+        if (!obj.__module) {
+          obj.__od = _this;
+          obj.__module = module;
+          obj.__modules = function(moduleId) {
+            return _this.module(moduleId);
+          }
+          if (parentModule) {
+            obj.__parent = parentModule;
+          }
+        }
+        if (depth >= 1) {
+          parentModule = obj;
+        }
+
         for (var key in obj) {
           if (!obj.hasOwnProperty(key) || (key == '__init'))
             continue;
           if ((typeof key !== 'string') || (key.charAt(0) != '_')) {
             if (obj[key] && (typeof obj[key] === 'object')) {
-              this.dispatch(false, data, obj[key], key, depth + 1, depth == 0 ? key : moduleId);
+              this.dispatchRun(obj[key], data, false, depth + 1, key, depth == 0 ? key : moduleId, module, parentModule);
             }
             else if (typeof obj[key] === 'function') {
-              obj[key]();
+              obj[key](obj);
             }
           }
         }
       }
     }
   }
-
 
   /*
    *  Utilities
@@ -158,8 +260,28 @@
   OdException.prototype.constructor = OdException;
 
 
+  //on DOM ready handler
+  //source: https://plainjs.com/
+  var onDomReady = function(callback) {
+    // in case the document is already rendered
+    if (document.readyState != 'loading') {
+      callback();
+    }
+    // modern browsers
+    else if (document.addEventListener) {
+      document.addEventListener('DOMContentLoaded', callback);
+    }
+    // IE <= 8
+    else {
+      document.attachEvent('onreadystatechange', function(){
+        if (document.readyState == 'complete')
+          callback();
+      });
+    }
+  }
+
   //$.extend in Vanilla JS
-  //source: http://youmightnotneedjquery.com/
+  //upgraded source: http://youmightnotneedjquery.com/
   var deepExtend = function(out) {
     out = out || {};
 
@@ -171,7 +293,7 @@
 
       for (var key in obj) {
         if (obj.hasOwnProperty(key)) {
-          if (typeof obj[key] === 'object')
+          if (obj[key] && (typeof obj[key] === 'object'))
             out[key] = deepExtend(out[key], obj[key]);
           else
             out[key] = obj[key];
